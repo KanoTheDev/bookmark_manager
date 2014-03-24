eb945d94ed82bdf6a4eb4c4126de5733eb9ae48a
KarolisNo <kano@kano.lt>
2014-03-24 16:07:55 +0000

    Switched to many to many for links and tags

--- server.rb
+++ server.rb
@@ -5,6 +5,7 @@ env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
+require './lib/tag'
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
@@ -17,9 +18,13 @@ class BookmarkManager < Sinatra::Application
 	end
 
 	post '/links' do
+
 		url = params["url"]
 		title = params["title"]
-		Link.create(:url => url, :title => title)
+		tags = params["tags"].split(" ").map do |tag|
+			Tag.first_or_create(:text => tag)
+		end
+		Link.create(:url => url, :title => title, :tags => tags)
 		redirect to('/')
 	end
 
